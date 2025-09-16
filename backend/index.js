bb3dbfff06f3bd577c206fd37c62556d096e5ff8
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-09-16 16:30:11 +0500

    removed listen get fron indexjs

--- backend/index.js
+++ backend/index.js
@@ -30,10 +30,6 @@ app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/listing", listingRouter);
 
-app.listen(3000, () => {
-  console.log("server is living on port 3000!");
-});
-
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
